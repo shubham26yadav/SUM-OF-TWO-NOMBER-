78168e22ae83a4baa35d9d1cd4d2826475ef570e
shubham yadav <115391493+shubham26yadav@users.noreply.github.com>
2022-10-09 12:54:35 +0530

    Add files via upload

---
+++ SUM OF TWO NOMBER .C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	int num1 ,num2,sum;
+    printf("Enter the number1 =");
+	scanf("%d",&num1);
+    printf("Enter the number2 =");
+	scanf("%d",&num2);
+	sum=num1+num2;
+	printf("Sum :%d",sum);
+	return 0;
+}
